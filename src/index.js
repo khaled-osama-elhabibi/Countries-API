e78602452f0efe1773d381ef724d239094f45926
Khaled Osama <khaled.osama.elhabibi@gmail.com>
2021-03-28 17:10:34 +0200

    a commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let root = document.getElementById('root') ;
+
+class Hi extends React.Component {
+    render(){
+        return (
+            <h1>rest-countries-api</h1>
+        ) ;
+    }
+}
+
+ReactDOM.render(
+    <Hi/>,
+    root
+)

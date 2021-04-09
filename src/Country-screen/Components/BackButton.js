c94b0799148a89ea29047d6a0b4b51bf28ad9e13
Khaled Osama <khaled.osama.elhabibi@gmail.com>
2021-04-09 17:45:23 +0200

    Test e

---
+++ src/Country-screen/Components/BackButton.js
@@ -0,0 +1,11 @@
+class BackButton extends React.Component{
+    render(){
+        return(
+            <div className = "back-container">
+                <span className = "back-button"><span class="material-icons">arrow_back</span>Back</span>
+            </div>
+        )
+    }
+}
+
+export default BackButton ;
--- src/Country-screen/index.js
+++ src/Country-screen/index.js
@@ -1,11 +1,13 @@
-import  Bar  from "../common-components/bar.js";
-import CountryCard from "./Components/CountryCard.js"
+import Bar  from "../common-components/bar.js" ;
+import CountryCard from "./Components/CountryCard.js" ;
+import BackButton from "./Components/BackButton.js" ;
 class CountryScreen extends React.Component{
     render(){
         return(
             <div className = "country-screen">
                 <Bar />
-                <CountryCard isoAlpha3Code="IRN"/>
+                <BackButton/>
+                <CountryCard isoAlpha3Code="EGY"/>
             </div>
         )
     }
